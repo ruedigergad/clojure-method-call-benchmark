aaa600c8c9b477bd72e445634efa73ecf7ebc5fd
Ruediger Gad <r.c.g@gmx.de>
2016-04-08 13:38:44 +0200

    Add map-rev benchmarks.

--- test/benchmark_test/method_call_tests.clj
+++ test/benchmark_test/method_call_tests.clj
@@ -404,6 +404,147 @@
         (f "bar")
         :verbose))))
 
+(deftest map-rev-benchmark-0
+  (println "\nRunning: map-rev-benchmark-0")
+  (let [m {:foo_0 0
+           :bar (fn [arg] (str arg))}]
+    (cc/with-progress-reporting
+      (cc/quick-bench
+        ((:bar m) "bar")
+        :verbose))))
+
+(deftest map-rev-benchmark-1
+  (println "\nRunning: map-rev-benchmark-1")
+  (let [m {:foo_0 0
+           :foo_1 1
+           :foo_2 2
+           :foo_3 3
+           :foo_4 4
+           :foo_5 5
+           :foo_6 6
+           :foo_7 7
+           :foo_8 8
+           :foo_9 9
+           :bar (fn [arg] (str arg))}]
+    (cc/with-progress-reporting
+      (cc/quick-bench
+        ((:bar m) "bar")
+        :verbose))))
+
+(deftest map-rev-benchmark-2
+  (println "\nRunning: map-rev-benchmark-2")
+  (let [m {:foo_0 0
+           :foo_1 1
+           :foo_2 2
+           :foo_3 3
+           :foo_4 4
+           :foo_5 5
+           :foo_6 6
+           :foo_7 7
+           :foo_8 8
+           :foo_9 9
+           :foo_10 10
+           :foo_11 11
+           :foo_12 12
+           :foo_13 13
+           :foo_14 14
+           :foo_15 15
+           :foo_16 16
+           :foo_17 17
+           :foo_18 18
+           :foo_19 19
+           :bar (fn [arg] (str arg))}]
+    (cc/with-progress-reporting
+      (cc/quick-bench
+        ((:bar m) "bar")
+        :verbose))))
+
+(deftest map-rev-benchmark-3
+  (println "\nRunning: map-rev-benchmark-3")
+  (let [m {:foo_0 0
+           :foo_1 1
+           :foo_2 2
+           :foo_3 3
+           :foo_4 4
+           :foo_5 5
+           :foo_6 6
+           :foo_7 7
+           :foo_8 8
+           :foo_9 9
+           :foo_10 10
+           :foo_11 11
+           :foo_12 12
+           :foo_13 13
+           :foo_14 14
+           :foo_15 15
+           :foo_16 16
+           :foo_17 17
+           :foo_18 18
+           :foo_19 19
+           :foo_20 20
+           :foo_21 21
+           :foo_22 22
+           :foo_23 23
+           :foo_24 24
+           :foo_25 25
+           :foo_26 26
+           :foo_27 27
+           :foo_28 28
+           :foo_29 29
+           :bar (fn [arg] (str arg))}]
+    (cc/with-progress-reporting
+      (cc/quick-bench
+        ((:bar m) "bar")
+        :verbose))))
+
+(deftest map-rev-benchmark-4
+  (println "\nRunning: map-rev-benchmark-4")
+  (let [m {:foo_0 0
+           :foo_1 1
+           :foo_2 2
+           :foo_3 3
+           :foo_4 4
+           :foo_5 5
+           :foo_6 6
+           :foo_7 7
+           :foo_8 8
+           :foo_9 9
+           :foo_10 10
+           :foo_11 11
+           :foo_12 12
+           :foo_13 13
+           :foo_14 14
+           :foo_15 15
+           :foo_16 16
+           :foo_17 17
+           :foo_18 18
+           :foo_19 19
+           :foo_20 20
+           :foo_21 21
+           :foo_22 22
+           :foo_23 23
+           :foo_24 24
+           :foo_25 25
+           :foo_26 26
+           :foo_27 27
+           :foo_28 28
+           :foo_29 29
+           :foo_30 30
+           :foo_31 31
+           :foo_32 32
+           :foo_33 33
+           :foo_34 34
+           :foo_35 35
+           :foo_36 36
+           :foo_37 37
+           :foo_38 38
+           :foo_39 39
+           :bar (fn [arg] (str arg))}]
+    (cc/with-progress-reporting
+      (cc/quick-bench
+        ((:bar m) "bar")
+        :verbose))))
+
 ;(deftest benchmark-1
 ;  (cc/report-result
 ;    (cc/quick-benchmark
