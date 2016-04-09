a66d3b93e137c6d0da7997a41444c85ccd786a19
Ruediger Gad <r.c.g@gmx.de>
2016-04-09 21:55:53 +0200

    Use addition as example instead of string concatenation.

--- test/benchmark_test/method_call_tests.clj
+++ test/benchmark_test/method_call_tests.clj
@@ -6,13 +6,13 @@
   (println "\nRunning: baseline-0")
   (cc/with-progress-reporting
     (cc/quick-bench
-      (str "foo" "bar")
+      (+ 1 2)
       :verbose)))
 
 (deftest baseline-1
   (println "\nRunning: baseline-1")
   (let [f (fn []
-            (str "foo"))]
+            2)]
     (cc/with-progress-reporting
       (cc/quick-bench
         (f)
@@ -21,57 +21,57 @@
 (deftest baseline-2
   (println "\nRunning: baseline-2")
   (let [f (fn [arg]
-            (str arg))]
+            arg)]
     (cc/with-progress-reporting
       (cc/quick-bench
-        (f "bar")
+        (f 2)
         :verbose))))
 
 (deftest baseline-3
   (println "\nRunning: baseline-3")
   (let [f (fn [arg]
-            (str "foo" arg))]
+            (+ 1 arg))]
     (cc/with-progress-reporting
       (cc/quick-bench
-        (f "bar")
+        (f 2)
         :verbose))))
 
 (deftest baseline-4
   (println "\nRunning: baseline-4")
-  (let [res "foo"
+  (let [res 1
         f (fn [arg]
-            (str res arg))]
+            (+ res arg))]
     (cc/with-progress-reporting
       (cc/quick-bench
-        (f "bar")
+        (f 2)
         :verbose))))
 
 (deftest condp-benchmark-0
   (println "\nRunning: condp-benchmark-0")
-  (let [res "foo"
+  (let [res 1
         f (fn [arg]
             (condp = arg
-              (str res arg)))]
+              (+ res arg)))]
     (cc/with-progress-reporting
       (cc/quick-bench
-        (f "bar")
+        (f 2)
         :verbose))))
 
 (deftest condp-benchmark-1
   (println "\nRunning: condp-benchmark-1")
-  (let [res "foo"
+  (let [res 1
         f (fn [arg]
             (condp = arg
               :foo_0 0
-              (str res arg)))]
+              (+ res arg)))]
     (cc/with-progress-reporting
       (cc/quick-bench
-        (f "bar")
+        (f 2)
         :verbose))))
 
 (deftest condp-benchmark-2
   (println "\nRunning: condp-benchmark-2")
-  (let [res "foo"
+  (let [res 1
         f (fn [arg]
             (condp = arg
               :foo_0 0
@@ -84,15 +84,15 @@
               :foo_7 7
               :foo_8 8
               :foo_9 9
-              (str res arg)))]
+              (+ res arg)))]
     (cc/with-progress-reporting
       (cc/quick-bench
-        (f "bar")
+        (f 2)
         :verbose))))
 
 (deftest condp-benchmark-3
   (println "\nRunning: condp-benchmark-3")
-  (let [res "foo"
+  (let [res 1
         f (fn [arg]
             (condp = arg
               :foo_0 0
@@ -115,15 +115,15 @@
               :foo_17 17
               :foo_18 18
               :foo_19 19
-              (str res arg)))]
+              (+ res arg)))]
     (cc/with-progress-reporting
       (cc/quick-bench
-        (f "bar")
+        (f 2)
         :verbose))))
 
 (deftest condp-benchmark-4
   (println "\nRunning: condp-benchmark-4")
-  (let [res "foo"
+  (let [res 1
         f (fn [arg]
             (condp = arg
               :foo_0 0
@@ -156,15 +156,15 @@
               :foo_27 27
               :foo_28 28
               :foo_29 29
-              (str res arg)))]
+              (+ res arg)))]
     (cc/with-progress-reporting
       (cc/quick-bench
-        (f "bar")
+        (f 2)
         :verbose))))
 
 (deftest condp-benchmark-5
   (println "\nRunning: condp-benchmark-5")
-  (let [res "foo"
+  (let [res 1
         f (fn [arg]
             (condp = arg
               :foo_0 0
@@ -207,18 +207,18 @@
               :foo_37 37
               :foo_38 38
               :foo_39 39
-              (str res arg)))]
+              (+ res arg)))]
     (cc/with-progress-reporting
       (cc/quick-bench
-        (f "bar")
+        (f 2)
         :verbose))))
 
 (deftest if-condp-benchmark-0
   (println "\nRunning: if-condp-benchmark-0")
-  (let [res "foo"
+  (let [res 1
         f (fn [arg]
             (if (not (keyword? arg))
-              (str res arg)
+              (+ res arg)
               (condp = arg
                 :foo_0 0
                 :foo_1 1
@@ -262,13 +262,13 @@
                 :foo_39 39)))]
     (cc/with-progress-reporting
       (cc/quick-bench
-        (f "bar")
+        (f 2)
         :verbose))))
 
 (deftest map-benchmark-0
   (println "\nRunning: map-benchmark-0")
-  (let [res "foo"
-        m {:bar (fn [arg] (str res arg))}]
+  (let [res 1
+        m {:bar (fn [arg] (+ res arg))}]
     (cc/with-progress-reporting
       (cc/quick-bench
         ((m :bar) "bar")
@@ -276,17 +276,17 @@
 
 (deftest map-benchmark-1
   (println "\nRunning: map-benchmark-1")
-  (let [res "foo"
+  (let [res 1
         m {:foo_0 0
-           :bar (fn [arg] (str res arg))}]
+           :bar (fn [arg] (+ res arg))}]
     (cc/with-progress-reporting
       (cc/quick-bench
-        ((m :bar) "bar")
+        ((m :bar) 2)
         :verbose))))
 
 (deftest map-benchmark-2
   (println "\nRunning: map-benchmark-2")
-  (let [res "foo"
+  (let [res 1
         m {:foo_0 0
            :foo_1 1
            :foo_2 2
@@ -297,15 +297,15 @@
            :foo_7 7
            :foo_8 8
            :foo_9 9
-           :bar (fn [arg] (str res arg))}]
+           :bar (fn [arg] (+ res arg))}]
     (cc/with-progress-reporting
       (cc/quick-bench
-        ((m :bar) "bar")
+        ((m :bar) 2)
         :verbose))))
 
 (deftest map-benchmark-3
   (println "\nRunning: map-benchmark-3")
-  (let [res "foo"
+  (let [res 1
         m {:foo_0 0
            :foo_1 1
            :foo_2 2
@@ -326,15 +326,15 @@
            :foo_17 17
            :foo_18 18
            :foo_19 19
-           :bar (fn [arg] (str res arg))}]
+           :bar (fn [arg] (+ res arg))}]
     (cc/with-progress-reporting
       (cc/quick-bench
-        ((m :bar) "bar")
+        ((m :bar) 2)
         :verbose))))
 
 (deftest map-benchmark-4
   (println "\nRunning: map-benchmark-4")
-  (let [res "foo"
+  (let [res 1
         m {:foo_0 0
            :foo_1 1
            :foo_2 2
@@ -365,15 +365,15 @@
            :foo_27 27
            :foo_28 28
            :foo_29 29
-           :bar (fn [arg] (str res arg))}]
+           :bar (fn [arg] (+ res arg))}]
     (cc/with-progress-reporting
       (cc/quick-bench
-        ((m :bar) "bar")
+        ((m :bar) 2)
         :verbose))))
 
 (deftest map-benchmark-5
   (println "\nRunning: map-benchmark-5")
-  (let [res "foo"
+  (let [res 1
         m {:foo_0 0
            :foo_1 1
            :foo_2 2
@@ -414,15 +414,15 @@
            :foo_37 37
            :foo_38 38
            :foo_39 39
-           :bar (fn [arg] (str res arg))}]
+           :bar (fn [arg] (+ res arg))}]
     (cc/with-progress-reporting
       (cc/quick-bench
-        ((m :bar) "bar")
+        ((m :bar) 2)
         :verbose))))
 
 (deftest map-cached-benchmark-0
   (println "\nRunning: map-cached-benchmark-0")
-  (let [res "foo"
+  (let [res 1
         m {:foo_0 0
            :foo_1 1
            :foo_2 2
@@ -463,35 +463,35 @@
            :foo_37 37
            :foo_38 38
            :foo_39 39
-           :bar (fn [arg] (str res arg))}
+           :bar (fn [arg] (+ res arg))}
         f (m :bar)]
     (cc/with-progress-reporting
       (cc/quick-bench
-        (f "bar")
+        (f 2)
         :verbose))))
 
 (deftest map-rev-benchmark-0
   (println "\nRunning: map-rev-benchmark-0")
-  (let [res "foo"
-        m {:bar (fn [arg] (str res arg))}]
+  (let [res 1
+        m {:bar (fn [arg] (+ res arg))}]
     (cc/with-progress-reporting
       (cc/quick-bench
-        ((:bar m) "bar")
+        ((:bar m) 2)
         :verbose))))
 
 (deftest map-rev-benchmark-1
   (println "\nRunning: map-rev-benchmark-1")
-  (let [res "foo"
+  (let [res 1
         m {:foo_0 0
-           :bar (fn [arg] (str res arg))}]
+           :bar (fn [arg] (+ res arg))}]
     (cc/with-progress-reporting
       (cc/quick-bench
-        ((:bar m) "bar")
+        ((:bar m) 2)
         :verbose))))
 
 (deftest map-rev-benchmark-2
   (println "\nRunning: map-rev-benchmark-2")
-  (let [res "foo"
+  (let [res 1
         m {:foo_0 0
            :foo_1 1
            :foo_2 2
@@ -502,15 +502,15 @@
            :foo_7 7
            :foo_8 8
            :foo_9 9
-           :bar (fn [arg] (str res arg))}]
+           :bar (fn [arg] (+ res arg))}]
     (cc/with-progress-reporting
       (cc/quick-bench
-        ((:bar m) "bar")
+        ((:bar m) 2)
         :verbose))))
 
 (deftest map-rev-benchmark-3
   (println "\nRunning: map-rev-benchmark-3")
-  (let [res "foo"
+  (let [res 1
         m {:foo_0 0
            :foo_1 1
            :foo_2 2
@@ -531,15 +531,15 @@
            :foo_17 17
            :foo_18 18
            :foo_19 19
-           :bar (fn [arg] (str res arg))}]
+           :bar (fn [arg] (+ res arg))}]
     (cc/with-progress-reporting
       (cc/quick-bench
-        ((:bar m) "bar")
+        ((:bar m) 2)
         :verbose))))
 
 (deftest map-rev-benchmark-4
   (println "\nRunning: map-rev-benchmark-4")
-  (let [res "foo"
+  (let [res 1
         m {:foo_0 0
            :foo_1 1
            :foo_2 2
@@ -570,15 +570,15 @@
            :foo_27 27
            :foo_28 28
            :foo_29 29
-           :bar (fn [arg] (str res arg))}]
+           :bar (fn [arg] (+ res arg))}]
     (cc/with-progress-reporting
       (cc/quick-bench
-        ((:bar m) "bar")
+        ((:bar m) 2)
         :verbose))))
 
 (deftest map-rev-benchmark-5
   (println "\nRunning: map-rev-benchmark-5")
-  (let [res "foo"
+  (let [res 1
         m {:foo_0 0
            :foo_1 1
            :foo_2 2
@@ -619,10 +619,10 @@
            :foo_37 37
            :foo_38 38
            :foo_39 39
-           :bar (fn [arg] (str res arg))}]
+           :bar (fn [arg] (+ res arg))}]
     (cc/with-progress-reporting
       (cc/quick-bench
-        ((:bar m) "bar")
+        ((:bar m) 2)
         :verbose))))
 
 (defprotocol MyProt
@@ -633,27 +633,27 @@
   [res]
   MyProt
   (foo [_] 0)
-  (bar [_ arg] (str res arg)))
+  (bar [_ arg] (+ res arg)))
 
 (deftest defrecord-benchmark-0
   (println "\nRunning: defrecord-benchmark-0")
-  (let [r (MyRec. "foo")]
+  (let [r (MyRec. 1)]
     (cc/with-progress-reporting
       (cc/quick-bench
-        (.bar r "bar")
+        (.bar r 2)
         :verbose))))
 
 (deftype MyType
   [res]
   MyProt
   (foo [_] 0)
-  (bar [_ arg] (str res arg)))
+  (bar [_ arg] (+ res arg)))
 
 (deftest deftype-benchmark-0
   (println "\nRunning: deftype-benchmark-0")
-  (let [t (MyType. "foo")]
+  (let [t (MyType. 1)]
     (cc/with-progress-reporting
       (cc/quick-bench
-        (.bar t "bar")
+        (.bar t 2)
         :verbose))))
 
