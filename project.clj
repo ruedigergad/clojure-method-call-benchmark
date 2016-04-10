1ffbec128802f79f89f8dd8470923362125d7fe4
Ruediger Gad <r.c.g@gmx.de>
2016-04-10 22:31:33 +0200

    Update project.clj.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject benchmark-test "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject clojure-method-call-benchmark "0.1.0-SNAPSHOT"
+  :description "Simple Set of Benchmarks for 'Clojure Objects' Method Calls"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
