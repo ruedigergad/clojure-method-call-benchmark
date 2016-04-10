907a067f101da779621dc5b2d06ccd3fcc38f882
Ruediger Gad <r.c.g@gmx.de>
2016-04-10 23:08:58 +0200

    Add stdout output.

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -2,9 +2,13 @@
 
 OUTPUT_NAME="$(date "+%Y-%m-%d_%H-%M")_$(hostname)_$(uname)"
 
+echo "Running benchmark..."
 lein test clojure-method-call-benchmark.benchmarks > ${OUTPUT_NAME}_raw_out.txt
 
 grep -e Benchmarking -e "Execution time mean" ${OUTPUT_NAME}_raw_out.txt | sed '$!N;s/\n/ /' | awk '{print $2,($7 * ($8 == "ns" ? 1 : 1000))}' | sort > tmp.out ; awk 'NR==FNR {if ($1 == "baseline-4") baseline = $2 ; next} {print $1,baseline,$2,($2/baseline)}' tmp.out tmp.out > ${OUTPUT_NAME}_out.txt
 
 rm -f tmp.out
+
+echo "Benchmark results:"
+cat ${OUTPUT_NAME}_out.txt
 
