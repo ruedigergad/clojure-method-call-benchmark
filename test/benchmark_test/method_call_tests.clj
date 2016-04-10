90dc8e586c752febf8186913caeb3c40b8119a1e
Ruediger Gad <r.c.g@gmx.de>
2016-04-10 18:12:04 +0200

    Use newly defined functions in benchmark.

--- test/benchmark_test/method_call_tests.clj
+++ test/benchmark_test/method_call_tests.clj
@@ -1,11 +1,10 @@
 (ns benchmark-test.method-call-tests
   (:require
+    [clojure-method-call-benchmark.core :refer :all]
     [clojure.test :refer :all]
     [criterium [core :as cc]])
   (:import
-    (clojure.lang IFn)
-    )
-  )
+    (clojure.lang IFn)))
 
 (deftest baseline-0
   (println "\nRunning: baseline-0")
@@ -53,221 +52,58 @@
 
 (deftest condp-benchmark-0
   (println "\nRunning: condp-benchmark-0")
-  (let [res 1
-        f (fn [arg]
-            (condp = arg
-              (+ res arg)))]
+  (let [o (condp-obj-0 1)]
     (cc/with-progress-reporting
       (cc/quick-bench
-        (f 2)
+        (o 2)
         :verbose))))
 
 (deftest condp-benchmark-1
   (println "\nRunning: condp-benchmark-1")
-  (let [res 1
-        f (fn [arg]
-            (condp = arg
-              :foo_0 0
-              (+ res arg)))]
+  (let [o (condp-obj-1 1)]
     (cc/with-progress-reporting
       (cc/quick-bench
-        (f 2)
+        (o 2)
         :verbose))))
 
 (deftest condp-benchmark-2
   (println "\nRunning: condp-benchmark-2")
-  (let [res 1
-        f (fn [arg]
-            (condp = arg
-              :foo_0 0
-              :foo_1 1
-              :foo_2 2
-              :foo_3 3
-              :foo_4 4
-              :foo_5 5
-              :foo_6 6
-              :foo_7 7
-              :foo_8 8
-              :foo_9 9
-              (+ res arg)))]
+  (let [o (condp-obj-2 1)]
     (cc/with-progress-reporting
       (cc/quick-bench
-        (f 2)
+        (o 2)
         :verbose))))
 
 (deftest condp-benchmark-3
   (println "\nRunning: condp-benchmark-3")
-  (let [res 1
-        f (fn [arg]
-            (condp = arg
-              :foo_0 0
-              :foo_1 1
-              :foo_2 2
-              :foo_3 3
-              :foo_4 4
-              :foo_5 5
-              :foo_6 6
-              :foo_7 7
-              :foo_8 8
-              :foo_9 9
-              :foo_10 10
-              :foo_11 11
-              :foo_12 12
-              :foo_13 13
-              :foo_14 14
-              :foo_15 15
-              :foo_16 16
-              :foo_17 17
-              :foo_18 18
-              :foo_19 19
-              (+ res arg)))]
+  (let [o (condp-obj-3 1)]
     (cc/with-progress-reporting
       (cc/quick-bench
-        (f 2)
+        (o 2)
         :verbose))))
 
 (deftest condp-benchmark-4
-  (println "\nRunning: condp-benchmark-4")
-  (let [res 1
-        f (fn [arg]
-            (condp = arg
-              :foo_0 0
-              :foo_1 1
-              :foo_2 2
-              :foo_3 3
-              :foo_4 4
-              :foo_5 5
-              :foo_6 6
-              :foo_7 7
-              :foo_8 8
-              :foo_9 9
-              :foo_10 10
-              :foo_11 11
-              :foo_12 12
-              :foo_13 13
-              :foo_14 14
-              :foo_15 15
-              :foo_16 16
-              :foo_17 17
-              :foo_18 18
-              :foo_19 19
-              :foo_20 20
-              :foo_21 21
-              :foo_22 22
-              :foo_23 23
-              :foo_24 24
-              :foo_25 25
-              :foo_26 26
-              :foo_27 27
-              :foo_28 28
-              :foo_29 29
-              (+ res arg)))]
-    (cc/with-progress-reporting
-      (cc/quick-bench
-        (f 2)
-        :verbose))))
+(println "\nRunning: condp-benchmark-4")
+(let [o (condp-obj-4 1)]
+  (cc/with-progress-reporting
+    (cc/quick-bench
+      (o 2)
+      :verbose))))
 
 (deftest condp-benchmark-5
   (println "\nRunning: condp-benchmark-5")
-  (let [res 1
-        f (fn [arg]
-            (condp = arg
-              :foo_0 0
-              :foo_1 1
-              :foo_2 2
-              :foo_3 3
-              :foo_4 4
-              :foo_5 5
-              :foo_6 6
-              :foo_7 7
-              :foo_8 8
-              :foo_9 9
-              :foo_10 10
-              :foo_11 11
-              :foo_12 12
-              :foo_13 13
-              :foo_14 14
-              :foo_15 15
-              :foo_16 16
-              :foo_17 17
-              :foo_18 18
-              :foo_19 19
-              :foo_20 20
-              :foo_21 21
-              :foo_22 22
-              :foo_23 23
-              :foo_24 24
-              :foo_25 25
-              :foo_26 26
-              :foo_27 27
-              :foo_28 28
-              :foo_29 29
-              :foo_30 30
-              :foo_31 31
-              :foo_32 32
-              :foo_33 33
-              :foo_34 34
-              :foo_35 35
-              :foo_36 36
-              :foo_37 37
-              :foo_38 38
-              :foo_39 39
-              (+ res arg)))]
+  (let [o (condp-obj-5 1)]
     (cc/with-progress-reporting
       (cc/quick-bench
-        (f 2)
+        (o 2)
         :verbose))))
 
-(deftest if-condp-benchmark-0
-  (println "\nRunning: if-condp-benchmark-0")
-  (let [res 1
-        f (fn [arg]
-            (if (not (keyword? arg))
-              (+ res arg)
-              (condp = arg
-                :foo_0 0
-                :foo_1 1
-                :foo_2 2
-                :foo_3 3
-                :foo_4 4
-                :foo_5 5
-                :foo_6 6
-                :foo_7 7
-                :foo_8 8
-                :foo_9 9
-                :foo_10 10
-                :foo_11 11
-                :foo_12 12
-                :foo_13 13
-                :foo_14 14
-                :foo_15 15
-                :foo_16 16
-                :foo_17 17
-                :foo_18 18
-                :foo_19 19
-                :foo_20 20
-                :foo_21 21
-                :foo_22 22
-                :foo_23 23
-                :foo_24 24
-                :foo_25 25
-                :foo_26 26
-                :foo_27 27
-                :foo_28 28
-                :foo_29 29
-                :foo_30 30
-                :foo_31 31
-                :foo_32 32
-                :foo_33 33
-                :foo_34 34
-                :foo_35 35
-                :foo_36 36
-                :foo_37 37
-                :foo_38 38
-                :foo_39 39)))]
+(deftest condp-if-benchmark-0
+  (println "\nRunning: condp-if-benchmark-0")
+  (let [o (condp-if-obj-0 1)]
     (cc/with-progress-reporting
       (cc/quick-bench
-        (f 2)
+        (o 2)
         :verbose))))
 
 (deftest map-benchmark-0
