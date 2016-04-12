a6a2f83748c14ad67941878394a05c64017bd0c0
Ruediger Gad <r.c.g@gmx.de>
2016-04-12 15:23:07 +0200

    Refactor result post-processing.

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -5,9 +5,13 @@ OUTPUT_NAME="$(date "+%Y-%m-%d_%H-%M")_$(hostname)_$(uname)"
 echo "Running benchmarks..."
 lein test clojure-method-call-benchmark.benchmarks > ${OUTPUT_NAME}_raw_out.txt
 
-grep -e Benchmarking -e "Execution time mean" ${OUTPUT_NAME}_raw_out.txt | sed '$!N;s/\n/ /' | awk '{print $2,($7 * ($8 == "ns" ? 1 : 1000))}' | sort > tmp.out ; awk 'NR==FNR {if ($1 == "baseline-4-fn-add-closure") baseline = $2 ; next} {print $1,baseline,$2,($2/baseline)}' tmp.out tmp.out > ${OUTPUT_NAME}_out.txt
+grep -e Benchmarking -e "Execution time mean" -e "Execution time std-deviation" ${OUTPUT_NAME}_raw_out.txt > tmp.out
 
-rm -f tmp.out
+awk '{if (NR%3 == 1 ) {ORS=" "; print $2} else if (NR%3 == 2) {ORS=" "; print ($5 * ($6 == "ns" ? 1 : 1000))} else {ORS="\n"; print ($5 * ($6 == "ns" ? 1 : 1000))}}' tmp.out | sort > tmp.out2
+
+awk 'NR==FNR {if ($1 == "baseline-4-fn-add-closure") baseline = $2 ; next} {print $1,baseline,$2,$3,($2/baseline)}' tmp.out2 tmp.out2 > ${OUTPUT_NAME}_out.txt
+
+rm -f tmp.out tmp.out2
 
 echo "Benchmark results:"
 cat ${OUTPUT_NAME}_out.txt
