8eaf60e2044ec86a85c779343c408e7e9149b383
Ruediger Gad <r.c.g@gmx.de>
2016-04-13 10:59:05 +0200

    Add pngs.

--- results/create_plots.sh
+++ results/create_plots.sh
@@ -3,3 +3,6 @@
 gnuplot -e "prefix='"$1"'" plot_1.gpl
 gnuplot -e "prefix='"$1"'" plot_2.gpl
 
+convert -flatten -density 150 -quality 100 -sharpen 0x1.0 ${1}_1.pdf ${1}_1.png
+convert -flatten -density 150 -quality 100 -sharpen 0x1.0 ${1}_2.pdf ${1}_2.png
+
